bdb596c21ed7589d6f421503b8d140ba9b9f2684
Janavi <jkumar@umail.ucsb.edu>
2018-07-11 18:34:15 -0700

    form fix

--- send_form_email.php
+++ send_form_email.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['email'])) {
  
-    $email_to = "jnkumar11@yourdomain.com";
+    $email_to = "jnkumar11@gmail.com";
     $email_subject = "Message from janavikumar.com";
  
     function died($error) {
@@ -16,9 +16,7 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
         !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
@@ -26,9 +24,7 @@ if(isset($_POST['email'])) {
      
  
     $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
     $comments = $_POST['comments']; // required
  
     $error_message = "";
@@ -44,9 +40,6 @@ if(isset($_POST['email'])) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
  
   if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
@@ -67,9 +60,7 @@ if(isset($_POST['email'])) {
      
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
 // create email headers
